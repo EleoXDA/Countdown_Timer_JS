ade2b537a8ad217205c1fca462be7846a239bf52
Elbay Malik <eleo.666@gmail.com>
2023-06-02 20:22:42 +0200

    Fix double timer and reset button behavior

--- script.js
+++ script.js
@@ -8,19 +8,20 @@ const timerDisplay = document.querySelector('#timer-display');
 let countdown;
 
 function timer(seconds) {
-    clearInterval(countdown);
+    const endTime = Date.now() + seconds * 1000;
 
-    const now = Date.now();
-    const then = now + seconds * 1000;
+    localStorage.setItem('endTime', endTime);
 
     displayTimeLeft(seconds);
 
     countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        const secondsLeft = Math.round((endTime - Date.now()) / 1000);
 
-        if(secondsLeft < 0) {
+        if (secondsLeft < 0) {
             clearInterval(countdown);
+            countdown = null;
             alert('Timer Finished!');
+            localStorage.removeItem('endTime');
             return;
         }
 
@@ -38,11 +39,17 @@ function displayTimeLeft(seconds) {
 }
 
 startButton.addEventListener('click', () => {
+    if (countdown) {
+      return;
+    }
     const hours = parseInt(hoursInput.value) || 0;
     const minutes = parseInt(minutesInput.value) || 0;
     const seconds = parseInt(secondsInput.value) || 0;
 
     if (hours >= 0 && minutes >= 0 && seconds >= 0) {
+        localStorage.setItem('hours', hours);
+        localStorage.setItem('minutes', minutes);
+        localStorage.setItem('seconds', seconds);
         timer((hours * 3600) + (minutes * 60) + seconds);
     } else {
         alert('Please enter valid time!');
@@ -55,4 +62,56 @@ resetButton.addEventListener('click', () => {
     secondsInput.value = '';
     timerDisplay.textContent = '00:00:00';
     clearInterval(countdown);
+    countdown = null;
+    localStorage.removeItem('endTime');
+    localStorage.removeItem('hours');
+    localStorage.removeItem('minutes');
+    localStorage.removeItem('seconds');
 });
+
+window.addEventListener('load', () => {
+    const savedHours = parseInt(localStorage.getItem('hours')) || 0;
+    const savedMinutes = parseInt(localStorage.getItem('minutes')) || 0;
+    const savedSeconds = parseInt(localStorage.getItem('seconds')) || 0;
+    const savedEndTime = parseInt(localStorage.getItem('endTime'));
+
+    if (savedEndTime) {
+        const timeLeft = Math.round((savedEndTime - Date.now()) / 1000);
+        if (timeLeft >= 0) {
+            hoursInput.value = savedHours;
+            minutesInput.value = savedMinutes;
+            secondsInput.value = savedSeconds;
+            timer(timeLeft);
+        } else {
+            localStorage.removeItem('endTime');
+        }
+    }
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var themeSelector = document.getElementById('theme-selector');
+  
+    themeSelector.addEventListener('change', function() {
+  
+      // Remove the existing theme class from the body
+      document.body.classList.remove('theme-default', 'theme-dark');
+  
+      // Add the selected theme class to the body
+      document.body.classList.add(this.value);
+  
+      // Save the selected theme in local storage
+      localStorage.setItem('selectedTheme', this.value);
+    });
+  
+    // Load the saved theme from local storage (if any)
+    var savedTheme = localStorage.getItem('selectedTheme');
+    if (savedTheme) {
+      // Remove the current theme from the body
+      document.body.classList.remove('theme-default', 'theme-dark');
+  
+      // Add the saved theme class to the body
+      document.body.classList.add(savedTheme);
+  
+      // Set the dropdown menu's value to the saved theme
+      themeSelector.value = savedTheme;
+    }
+  });
